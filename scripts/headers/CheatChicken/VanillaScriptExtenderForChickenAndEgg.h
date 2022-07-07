0a34707e72b25d3cd18f959dd7a22f57ff12d717
Mrowr Purr <mrowr.purr@gmail.com>
2022-07-07 06:30:07 -0700

    Rename vanilla script extender header

---
+++ scripts/headers/CheatChicken/VanillaScriptExtenderForChickenAndEgg.h
@@ -0,0 +1,33 @@
+#pragma once
+
+/*
+    Extensions to the original KTSMOKE script.
+
+    Cheat Chicken is *evil* and hijacks this script :)
+*/
+
+#define DEBUG_NAME "CheatChicken KTSMOKE"
+#include "Debug.h"
+
+#include "CheatChicken/Messages.h"
+
+#define is_script_attached_to_cheat_chicken_or_egg true
+
+variable loaded = false;
+
+procedure load begin
+    debug("Egg - I AM ALIVE!");
+end
+
+procedure start begin
+    if not loaded then begin
+        loaded = true;
+        call load;
+    end
+end
+
+procedure look_at_p_proc begin
+    script_overrides;
+     // TODO: from random group with variants for if you've seen it before and number of game days like it'll get rotten :)
+    display_msg(mstr(MSG_LOOKAT_UNFAMILIAR));
+end
