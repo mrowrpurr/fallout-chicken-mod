a7afa3d965926459a53d4b9def72fe46efdbef81
Mrowr Purr <mrowr.purr@gmail.com>
2022-07-08 15:26:11 -0700

    export data/config --> available for other scripts
    Needed to access data/config from our EggOrChickenScript

--- scripts/headers/CheatChicken/EggOrChickenScript.h
+++ scripts/headers/CheatChicken/EggOrChickenScript.h
@@ -7,7 +7,25 @@
 #include "command.h"
 #include "CheatChicken.h"
 
-procedure drop_p_proc begin
-    display_msg("YOU DROPPED THE EGG");
+import variable cheatchicken_config_exported;
+import variable cheatchicken_data_exported;
+
+#define MSG_EGG_NAME 101
+#define MSG_YOU_SEE  102
+
+procedure look_at_p_proc begin
+    variable map = cheatchicken_config_exported;
     script_overrides;
+    display_msg("import DATA: " + cheatchicken_data_exported);
+    display_msg("import CONFIG: " + cheatchicken_config_exported);
+    display_msg("SCRIPT_ID: " + map.Egg.script_id);
+    // variable script_id = get_script(self_obj);
+    display_msg(
+        message_str(map.Egg.script_id, MSG_YOU_SEE) +  message_str(map.Egg.script_id, MSG_EGG_NAME)
+    );
+end
+
+procedure drop_p_proc begin
+    script_overrides;
+    display_msg("YOU DROPPED THE EGG");
 end
