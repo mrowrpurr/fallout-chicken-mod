59b76ff4dfeb32ab175968df1bf04cf0f7286b5d
Mrowr Purr <mrowr.purr@gmail.com>
2022-07-08 20:48:53 -0700

    WIP

--- scripts/headers/CheatChicken/EggOrChickenScript.h
+++ scripts/headers/CheatChicken/EggOrChickenScript.h
@@ -5,11 +5,13 @@
 
 #include "define.h"
 #include "command.h"
+#include "sfall/sfall.h"
 #include "sfall/lib.arrays.h"
 #include "sfall/lib.strings.h"
 #include "CheatChicken.h"
 #include "CheatChicken/Messages.h"
 #include "FormattedDebug.h"
+#include "FormattedDisplay.h"
 
 import variable cheatchicken_config;
 import variable cheatchicken_data;
@@ -22,6 +24,11 @@ procedure start begin
     data   = cheatchicken_data;
 end
 
+procedure critter_p_proc begin
+    if self_obj == data.chicken_obj then
+        animate_move_to_tile(Future_Distance_From_Dude(2));
+end
+
 procedure look_at_p_proc begin
     script_overrides;
     display_msg("LOOK AT...");
@@ -32,10 +39,26 @@ procedure spawn_chicken begin
     variable config_section_name, config_section;
     foreach config_section_name: config_section in config begin
         if string_starts_with(config_section_name, "ReplacementCharacter:") then begin
-            debug_array(config_section);
-            display_msg("SPAWN " + atoi(config_section.pid) + " with script " + atoi(config_section.script_id));
-            data.chicken_obj = create_object_sid(atoi(config_section.pid), 0, 0, atoi(config_section.script_id));
+            // Found the replacement character - TODO check to see we're not on its home map(s)
+            variable character_info = config_section;
+
+            // Replace the .frm(s) of the character with our configured .frm(s) for this egg's chicken
+            variable chicken_name = config.Egg.hatches;
+            variable chicken_info = config["Chicken:" + chicken_name];
+            variable chicken_idle_path = chicken_info.idle_animation;
+            variable character_idle_path = sprintf("art\\critters\\%s", character_info.idle_animation);
+
+            variable file = fs_copy("MOVE.FRM", character_idle_path);
+            // variable file = fs_copy(character_idle_path, "MOVE.FRM");
+            // variable file = fs_copy(character_idle_path, character_idle_path);
+            display2("character, chicken", character_idle_path, chicken_idle_path);
+
+            data.chicken_obj = create_object_sid(atoi(character_info.pid), 0, 0, atoi(character_info.script_id));
+
             critter_attempt_placement(data.chicken_obj, dude_tile, dude_elevation);
+
+            party_add(data.chicken_obj);
+
             break;
         end
     end
